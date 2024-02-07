9341c1a2f3e42576a09c7f52feb6bc9e2e3e49f9
Akshat <akshatm1011@gmail.com>
2024-02-07 10:58:52 -0800

    added some small changes

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ https://github.com/openai/gpt-2/blob/master/src/model.py
 2) huggingface/transformers PyTorch implementation:
 https://github.com/huggingface/transformers/blob/main/src/transformers/models/gpt2/modeling_gpt2.py
 """
-
+ 
 import math
 import inspect
 from dataclasses import dataclass
