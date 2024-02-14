5c017e1c0b25245c437e4e3a72b631f7668e3ba5
Sujen Kancherla <sujenk@gmail.com>
2024-02-13 17:44:24 -0800

    change tuning script

--- hyper_parameter_script.py
+++ hyper_parameter_script.py
@@ -10,7 +10,11 @@ params = list(product(learning_rates, dropouts, n_layers))
 
 counter = 0
 for lr, dropout, n_layer in params:
-    command = f'python3 train.py --compile=False --batch_size=4 --eval_interval=50 --learning_rate={lr:.9f} --min_lr={lr/10:.9f} --dropout={dropout} --n_layer={n_layer} --ckpt_name=ckpt{counter}.pt'
+    command = f'python3 train.py --compile=False --wandb_log=True --batch_size=4 --max_iters=50 --eval_interval=50 --loss_func="squentropy" --learning_rate={lr:.9f} --min_lr={lr/10:.9f} --dropout={dropout} --n_layer={n_layer} --ckpt_name=ckpt{counter}.pt'
     #print(command)
     os.system(command)
+    
+    move_command = f'scp out/ckpt{counter}.pt skancherla@dsmlp-login.ucsd.edu:/dsmlp/workspaces-fs04/DSC180A_FA23_A00/teams/b13/group1/ckpt{counter}.pt'
+    os.system(move_command)
     counter += 1
+    break
