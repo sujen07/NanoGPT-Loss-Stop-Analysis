e0c90b7966245bd6aacd3eba8f4d94c22bf97e0d
Sujen Kancherla <sujenk@gmail.com>
2024-02-15 16:11:37 -0800

    change out_dir to teams folder

--- hyper_parameter_script.py
+++ hyper_parameter_script.py
@@ -7,14 +7,14 @@ n_layers = [8, 12, 16]
 
 
 params = list(product(learning_rates, dropouts, n_layers))
+path = os.getcwd().split('/')[:3]
+path += ['teams', 'b13', 'group1']
+out = os.path.join(*path)
 
 counter = 0
 for lr, dropout, n_layer in params:
-    command = f'python3 train.py --compile=False --wandb_log=True --batch_size=4 --max_iters=50 --eval_interval=50 --loss_func="squentropy" --learning_rate={lr:.9f} --min_lr={lr/10:.9f} --dropout={dropout} --n_layer={n_layer} --ckpt_name=ckpt{counter}.pt'
+    command = f'python3 train.py --compile=False --wandb_log=True --out_dir={out} --batch_size=4 --max_iters=50 --eval_interval=50 --loss_func="squentropy" --learning_rate={lr:.9f} --min_lr={lr/10:.9f} --dropout={dropout} --n_layer={n_layer} --ckpt_name=ckpt{counter}.pt'
     #print(command)
     os.system(command)
     
-    move_command = f'scp out/ckpt{counter}.pt skancherla@dsmlp-login.ucsd.edu:/dsmlp/workspaces-fs04/DSC180A_FA23_A00/teams/b13/group1/ckpt{counter}.pt'
-    os.system(move_command)
     counter += 1
-    break
